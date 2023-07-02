ebd4368ee7886f345dcf0fe9e32fff9980230283
LitFill <98679748+LitFill@users.noreply.github.com>
2023-07-02 21:52:43 +0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,31 @@
+const elemenInput = document.getElementById("input-pembaca");
+const tombolHitung = document.getElementById("tombol-hitung");
+const elemenHasil = document.getElementById("p-hasil");
+
+const jumlahYasin = 41;
+
+tombolHitung.addEventListener("click", hitung);
+
+function hitung() {
+    const jumlahPembaca = elemenInput.value;
+    const pembacaKurangSatu =
+        jumlahPembaca * Math.ceil(jumlahYasin / jumlahPembaca) - jumlahYasin;
+    const jumlahBacaanNormal = Math.ceil(jumlahYasin / jumlahPembaca);
+    const jumlahPembacaNormal = jumlahPembaca - pembacaKurangSatu;
+    const hasilHitungan = `${jumlahPembacaNormal} orang membaca ${jumlahBacaanNormal} kali,\n${pembacaKurangSatu} orang membaca ${
+        jumlahBacaanNormal - 1
+    } kali.`;
+    const detailHitungan = `(${jumlahPembacaNormal} * ${jumlahBacaanNormal} + ${pembacaKurangSatu} * ${
+        jumlahBacaanNormal - 1
+    }) = (${jumlahPembacaNormal * jumlahBacaanNormal} + ${
+        pembacaKurangSatu * (jumlahBacaanNormal - 1)
+    }) = ${
+        jumlahPembacaNormal * jumlahBacaanNormal +
+        pembacaKurangSatu * (jumlahBacaanNormal - 1)
+    }`;
+
+    console.log(hasilHitungan);
+    console.log(detailHitungan);
+
+    elemenHasil.innerText = hasilHitungan;
+}
