42ea1a0d64e146baea642804146fc9e026e386db
LitFill <98679748+LitFill@users.noreply.github.com>
2023-07-04 21:58:40 +0700

    Update main.js
    add handle for when jumlahPembaca less than 1.

--- main.js
+++ main.js
@@ -5,27 +5,31 @@ const elemenHasil = document.getElementById("p-hasil");
 const jumlahYasin = 41;
 
 tombolHitung.addEventListener("click", hitung);
+elemenInput.addEventListener("keyup", hitung);
 
 function hitung() {
     const jumlahPembaca = elemenInput.value;
-    const pembacaKurangSatu =
-        jumlahPembaca * Math.ceil(jumlahYasin / jumlahPembaca) - jumlahYasin;
-    const jumlahBacaanNormal = Math.ceil(jumlahYasin / jumlahPembaca);
-    const jumlahPembacaNormal = jumlahPembaca - pembacaKurangSatu;
-    const hasilHitungan = `${jumlahPembacaNormal} orang membaca ${jumlahBacaanNormal} kali,\n${pembacaKurangSatu} orang membaca ${
-        jumlahBacaanNormal - 1
-    } kali.`;
-    const detailHitungan = `(${jumlahPembacaNormal} * ${jumlahBacaanNormal} + ${pembacaKurangSatu} * ${
-        jumlahBacaanNormal - 1
-    }) = (${jumlahPembacaNormal * jumlahBacaanNormal} + ${
-        pembacaKurangSatu * (jumlahBacaanNormal - 1)
-    }) = ${
-        jumlahPembacaNormal * jumlahBacaanNormal +
-        pembacaKurangSatu * (jumlahBacaanNormal - 1)
-    }`;
-
-    console.log(hasilHitungan);
-    console.log(detailHitungan);
-
-    elemenHasil.innerText = hasilHitungan;
+    if (jumlahPembaca > 0) {
+        const pembacaKurangSatu = jumlahPembaca * Math.ceil(jumlahYasin / jumlahPembaca) - jumlahYasin;
+        const jumlahBacaanNormal = Math.ceil(jumlahYasin / jumlahPembaca);
+        const jumlahPembacaNormal = jumlahPembaca - pembacaKurangSatu;
+        const hasilHitungan = `${jumlahPembacaNormal} orang membaca ${jumlahBacaanNormal} kali,\n${pembacaKurangSatu} orang membaca ${
+            jumlahBacaanNormal - 1
+        } kali.`;
+        const detailHitungan = `(${jumlahPembacaNormal} * ${jumlahBacaanNormal} + ${pembacaKurangSatu} * ${
+            jumlahBacaanNormal - 1
+        }) = (${jumlahPembacaNormal * jumlahBacaanNormal} + ${
+            pembacaKurangSatu * (jumlahBacaanNormal - 1)
+        }) = ${
+            jumlahPembacaNormal * jumlahBacaanNormal +
+            pembacaKurangSatu * (jumlahBacaanNormal - 1)
+        }`;
+    
+        console.log(hasilHitungan);
+        console.log(detailHitungan);
+    
+        elemenHasil.innerText = hasilHitungan;
+    } else {
+        elemenHasil.innerText = "Maaf, jumlah pembaca tidak boleh kurang dari 1.";
+    }
 }
