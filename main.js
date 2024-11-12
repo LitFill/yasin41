9eeed0ff598eea2d7c0289c30ce3020814c28756
LitFill <marrazzy54@gmail.com>
2024-11-13 00:26:08 +0700

    refactor(js): revert the if so can use early return

--- main.js
+++ main.js
@@ -6,36 +6,38 @@ const jumlahYasin = 41;
 
 tombolHitung.addEventListener("click", hitung);
 elemenInput.addEventListener("keyup", hitung);
-elemenInput.addEventListener("change", hitung);
 
 function hitung() {
     const jumlahPembaca = elemenInput.value;
-    if (jumlahPembaca > 0) {
-        const pembacaKurangSatu =
-            jumlahPembaca * Math.ceil(jumlahYasin / jumlahPembaca) -
-            jumlahYasin;
-        const jumlahBacaanNormal = Math.ceil(jumlahYasin / jumlahPembaca);
-        const jumlahPembacaNormal = jumlahPembaca - pembacaKurangSatu;
-        const hasilHitungan =
-            `${jumlahPembacaNormal} orang membaca ${jumlahBacaanNormal} kali,\n${pembacaKurangSatu} orang membaca ${
-                jumlahBacaanNormal - 1
-            } kali.`;
-        const detailHitungan =
-            `(${jumlahPembacaNormal} * ${jumlahBacaanNormal} + ${pembacaKurangSatu} * ${
-                jumlahBacaanNormal - 1
-            }) = (${jumlahPembacaNormal * jumlahBacaanNormal} + ${
-                pembacaKurangSatu * (jumlahBacaanNormal - 1)
-            }) = ${
-                jumlahPembacaNormal * jumlahBacaanNormal +
-                pembacaKurangSatu * (jumlahBacaanNormal - 1)
-            }`;
-
-        console.log(hasilHitungan);
-        console.log(detailHitungan);
-
-        elemenHasil.innerText = hasilHitungan;
-    } else {
+    if (jumlahPembaca < 1) {
         elemenHasil.innerText =
             "Maaf, jumlah pembaca tidak boleh kurang dari 1.";
+        return;
     }
+
+    const pembacaKurangSatu =
+        jumlahPembaca * Math.ceil(jumlahYasin / jumlahPembaca) -
+        jumlahYasin;
+
+    const jumlahBacaanNormal = Math.ceil(jumlahYasin / jumlahPembaca);
+    const jumlahPembacaNormal = jumlahPembaca - pembacaKurangSatu;
+    const hasilHitungan =
+        `${jumlahPembacaNormal} orang membaca ${jumlahBacaanNormal} kali,\n${pembacaKurangSatu} orang membaca ${
+            jumlahBacaanNormal - 1
+        } kali.`;
+
+    const detailHitungan =
+        `(${jumlahPembacaNormal} * ${jumlahBacaanNormal} + ${pembacaKurangSatu} * ${
+            jumlahBacaanNormal - 1
+        }) = (${jumlahPembacaNormal * jumlahBacaanNormal} + ${
+            pembacaKurangSatu * (jumlahBacaanNormal - 1)
+        }) = ${
+            jumlahPembacaNormal * jumlahBacaanNormal +
+            pembacaKurangSatu * (jumlahBacaanNormal - 1)
+        }`;
+
+    console.log(hasilHitungan);
+    console.log(detailHitungan);
+
+    elemenHasil.innerText = hasilHitungan;
 }
